598400e2fd4659611df92ec73c776e816a47a47d
dderible <164251032+dderible@users.noreply.github.com>
2024-10-12 21:18:29 -0230

    Added Inserted Info (problem 2)

--- problem2.sql
+++ problem2.sql
@@ -30,3 +30,35 @@ CREATE TABLE order_items (
 	FOREIGN KEY (product_id) REFERENCES products(id),
 	PRIMARY KEY (order_id, product_id)
 );
+
+--INSERTED INFORMATION
+INSERT INTO products (product_name, price, stock_quantity) VALUES
+	('Laptop', 999.99, 20),
+	('Headphones', 59.99, 80),
+	('Nintendo Switch', 399.99, 65),
+	('Monitor', 149.99, 12),
+	('Speaker', 109.99, 30);
+
+INSERT INTO customers (first_name, last_name, email) VALUES
+	('Declan', 'Derible', 'declan.derible@keyin.com'),
+	('Evan', 'Morris', 'evan.morris@keyin.com'),
+	('Logan', 'Janes', 'logan.janes@keyin.com'),
+	('Gavin', 'Janes', 'gavin.janes@keyin.com');
+
+INSERT INTO orders (customer_id, order_date)
+	(1, '2024-10-12'),
+	(2, '2024-09-28'),
+	(3, '2023-04-20'),
+	(4, '2024-02-03'),
+	(5, '2024-07-01');
+
+INSERT INTO order_items (order_id, product_id, quantity) VALUES
+	(1, 1, 1),
+	(1, 3, 1),
+	(2, 5, 1),
+	(2, 1, 2),
+	(3, 2, 2),
+	(3, 4, 1),
+	(4, 4, 4),
+	(4, 3, 1);
+
