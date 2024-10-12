c3c8766afb25c1f8a5a425d89d1908ee9492d50c
dderible <164251032+dderible@users.noreply.github.com>
2024-10-12 19:41:39 -0230

    Added Inserted Info (problem 1)

--- problem1.sql
+++ problem1.sql
@@ -31,3 +31,29 @@ CREATE TABLE enrollments (
     FOREIGN KEY (student_id) REFERENCES students(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
+
+--INSERTED INFORMATION
+INSERT INTO students (first_name, last_name, email, school_enrollment_date) VALUES
+	('Declan', 'Derible', 'declan.derible@keyin.com', '2024-03-16'),
+	('Evan', 'Morris', 'evan.morris@keyin.com', '2024-01-28'),
+	('Aaron', 'Saunders', 'aaron.saunders@keyin.com', '2023-12-12'),
+	('Logan', 'Janes', 'logan.janes@keyin.com', '2024-05-20'),
+	('Gavin', 'Janes', 'gavin.janes@keyin.com', '2024-05-20');
+
+INSERT INFO professors (first_name, last_name, department) VALUES
+	('Sam', 'Martino', 'Website Development'),
+	('Sebastian', 'More', 'AWS Development'),
+	('Jackie', 'Chamorro', 'Java Development'),
+	('Katia', 'Stone', 'UI/UX Development');
+
+INSERT INFO courses (course_name, course_description, professor_id) VALUES
+	('Website Development', 'Introduction to Web Dev', 1),
+	('AWS Development', 'Introduction to Amazon Web Services', 2),
+	('UI/UX Development', 'Introduction to UI/UX Design', 3);
+
+INSERT INFO enrollments (student_id, course_id, enrollment_date) VALUES
+	(1, 1, '2024-03-16'),
+	(2, 1, '2024-01-28'),
+	(3, 3, '2023-12-12'),
+	(4, 2, '2024-05-20'),
+	(5, 2, '2024-05-20');
